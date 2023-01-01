e38bfdc87c6d337a7568fd98179989b0626f61b1
RuhRuh66 <toruh@deluxe.ocn.ne.jp>
2023-01-02 07:39:43 +0900

    変更

--- Maze_Master.py
+++ Maze_Master.py
@@ -2,10 +2,19 @@ H, W = map(int, input().split())
 maze = []
 
 for i in range(H):
-    j = list(input())
-    maze.append(j)
-
-
-def move_count(start_point, end_point):
+    temp = list(input())
+    maze.append(temp)
     
+from collections import deque
+
+def explore(x, y):
+    maze_count = [[-1] * W for _ in range(H)]
+    que = deque()
+    
+    maze_count[x][y] = 0
+    que.append([x, y])
+    
+    while len(que) > 0:
+        now_point = que.popleft()
+        
     
