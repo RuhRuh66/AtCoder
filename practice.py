2490ba9f1adffd6858e3ba0e2d3bcb98d3ad4dce
RuhRuh66 <toruh@deluxe.ocn.ne.jp>
2023-01-27 07:47:10 +0900

    add practice.py to gitignore

--- practice.py
+++ practice.py
@@ -1,23 +1,41 @@
-H, N = map(int, input().split())
-magics = []
-for i in range(N):
-    temp = list(map(int, input().split()))
-    magics.append(temp)
+def segfunc(x, y):
+    return x+y
 
-inf = 10 ** 10
-DP = [[inf] * (H+1) for i in range(N+1)]    
-DP[0][0] = 0
-
-for i in range(N):
-    for j in range(H+1):
-        if DP[i][j] == inf:
-            continue
+class SegTree():
+    def __init__(self, x_list, init, segfunc):
+        self.x_list = x_list
+        self.init = init
+        self.segfunc = segfunc
+        self.height = len(x_list).bit_length() + 1
+        self.tree = [init] * (2**self.height)
+        self.num = 2 ** (self.height-1)
+        for i in range(len(x_list)):
+            self.tree[2**(self.height-1) + i] = x_list[i]
+        for i in range(self.num-1, 0, -1):
+            self.tree[i] = self.segfunc(self.tree[2*i], self.tree[2*i+1])
+            
+if __name__ == '__main__':
+    a = [5, 7, 8]
+    n = SegTree(a, 0, segfunc)
+    print(n.height)
+    print(n.num)
+    print(n.tree)
+    print(n.x_list)
+    print(n.tree[n.num:])
+    
         
-        DP[i+1][j] = min(DP[i][j], DP[i+1][j])
-
-        if j + magics[i][0] >= H:
-            DP[i+1][H] = DP[i][j] + magics[i][1]
-        else:
-            DP[i+1][j+magics[i][1]] = min(DP[i][j]+magics[i+1][1])
-
-
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
