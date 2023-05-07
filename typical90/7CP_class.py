4771e979da6407018325f81553b68a3e79d76e49
RuhRuh66 <toruh@deluxe.ocn.ne.jp>
2023-05-08 08:16:06 +0900

    CP_classes added

--- typical90/7CP_class.py
+++ typical90/7CP_class.py
@@ -1,33 +1,30 @@
 N = int(input())
-edges = [[] for i in range(N)]
-for j in range(N-1):
-    A, B = map(int, input().split())
-    A -= 1
-    B -= 1
-    edges[A].append(B)
-    edges[B].append(A)
+A = list(map(int, input().split()))
 
+A.sort()
 
-dist = [0] * N
+Q = int(input())
 
-def dfs(x, last=-1):
-    global dist
-    for to in edges[x]:
-        if to == last:
-            continue
-        dist[to] = dist[x] + 1
-        dfs(to, x)
-    return dist
+for i in range(Q):
+    b = int(input())
+    ans = 0
+    if b <= A[0]:
+        ans = A[0]-b
+    elif b >= A[N-1]:
+        ans = b - A[N-1]
+    else:
+        ok = -1
+        ng = N
+        while ng - ok > 1:
+            mid = (ok + ng) //  2
+            if b <= A[mid]:
+                ng = mid
+            else:
+                ok = mid
+        ans = min(b-A[ok], A[ng]-b) 
+    print(ans)
+            
         
-s = dfs(0)
-max_dist = max(s)
-max_dist_idx = dist.index(max_dist)
-
-dist[max_dist_idx] = 0
-t = dfs(max_dist_idx)
-
-print(max(t) + 1)
-
 
 
 
