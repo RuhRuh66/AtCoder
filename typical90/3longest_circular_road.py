2df7e4fb032f54e5c28823911b9cbe4d89be4517
Toru hara <toruh@deluxe.ocn.ne.jp>
2023-05-09 07:48:21 +0900

    longest circular road file added

--- typical90/3longest_circular_road.py
+++ typical90/3longest_circular_road.py
@@ -1,46 +1,26 @@
 import sys
 sys.setrecursionlimit(10**6)
 
-N = int(input())
+N  = int(input())
+dest = [[] for i in range(N)]
+for i in range(1, N):
+    A, B = map(int, input().split())
+    A -= 1
+    B -= 1
 
-edges = [[] for i in range(N)]
-
-for i in range(N-1):
-    x, y = map(int, input().split())
-    x, y = x-1, y-1
-    edges[x].append(y)
-    edges[y].append(x)
+    dest[A].append(B)
+    dest[B].append(A)
     
-
-dist = [0] * N
-
+distance = [0] * N
+visited = [False] * N
 
 def dfs(x, last=-1):
-    global dist
-    for to in edges[x]:
-        if to == last:
+    visited[x] = True
+    for to in dest[x]:
+        if visited[to] == True:
             continue
-        dist[to] = dist[x] + 1
+
+x        distance[to] = distance[x] + 1
         dfs(to, x)
-        
-dfs(0)
-max_dist = max(dist)
-farest = dist.index(max_dist)
-
-dist[farest] = 0
-dfs(farest)
-
-print(max(dist)+1)
 
 
-
-            
-        
-
-    
-    
-    
-
-    
-
-                
