609d6e0e7fc2fe8eaa49fec59fceb610bfb5ba1d
Toru hara <toruh@deluxe.ocn.ne.jp>
2023-05-23 07:59:30 +0900

    modified atcounter and atcounter

--- typical90/8Atcounter.py
+++ typical90/8Atcounter.py
@@ -1,18 +1,5 @@
-N = int(input())
-S = [0]+list(input())
+n = int(input())
+s = str(input())
 
-T = '0atcoder'
-mod = 10**9 +7
+atcoder = 'atcoder'
 
-DP = [[0]*8 for i in range(N+1)]
-DP[0][0] = 1
-for i in range(1, N+1):
-    DP[i][0] = 1
-
-for i in range(1, N+1):
-    for j in range(1, len(T)):
-        DP[i][j] = DP[i-1][j]
-        if S[i] == T[j]:
-            DP[i][j] = DP[i-1][j-1] + DP[i-1][j]
-            
-print((DP[N][7])%mod)
