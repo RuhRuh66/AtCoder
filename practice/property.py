b553e921b857fb02b14ffbcccfc82b32b079e323
RuhRuh66 <toruh@deluxe.ocn.ne.jp>
2023-07-28 12:28:11 +0900

    property.py is the practice of dfs of alog-method

--- practice/property.py
+++ practice/property.py
@@ -1,39 +1,45 @@
 import sys
 sys.setrecursionlimit(10**6)
 
-N, M = map(int, input().split())
-A = [[] for _ in range(N)]
-for i in range(M):
-    a, b = map(int, input().split())
-    A[a].append(b)
-    A[b].append(a)
-    
-for j in A:
-    j.sort()
+H, W = map(int, input().split())
 
+A = [list(input()) for _ in range(H)]
 
-visited = [False] * N
-def rec(v, Gv, seen):
+move = [(0, 1), (-1, 0), (0, -1), (1, 0)]
 
+grid = [[] for i in range(H*W)]
+
+def getnum(x, y, H, W):
+    return x*W + y
+
+def rec(v, dv, seen):
     seen[v] = True
-
-    
-    for n in Gv[v]:
+    for n in dv[v]:
         if seen[n] == True:
             continue
-        rec(n, Gv, seen)
-        
-    return 
- 
-rec(0, A, visited)
-cnt = 1
-for i in range(N):
-    if visited[i] == False:
-        rec(i, A, visited)
-        cnt += 1
-        
-print(cnt)
-        
+        rec(n, dv, seen)
+    return
     
-    
-    
+def makegrid(A, grid, H, W, move):
+    for i in range(H):
+        for j in range(W):
+            if A[i][j] == '.':
+                continue
+            for dx, dy in move:
+                nx, ny = i+dx, j+dy
+                if 0<= nx <H and 0 <= ny < W and A[nx][ny] == '#':
+                    grid[getnum(i, j, H, W)].append(getnum(nx, ny, H, W))
+    return grid
+                    
+visited = [False] * (H*W)
+
+dv = makegrid(A, grid, H , W, move)
+
+for i in range(H):
+    for j in range(W):
+        if A[i][j] == '.':
+            continue
+        rec(getnum(i, j, H, W), dv, visited)
+
+print(visited)
+        
