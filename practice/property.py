ff60f7f8213aa45f0279c6173b3e270eb106be88
RuhRuh66 <toruh@deluxe.ocn.ne.jp>
2023-07-29 08:53:27 +0900

    property.py was completed

--- practice/property.py
+++ practice/property.py
@@ -2,44 +2,47 @@ import sys
 sys.setrecursionlimit(10**6)
 
 H, W = map(int, input().split())
-
 A = [list(input()) for _ in range(H)]
 
-move = [(0, 1), (-1, 0), (0, -1), (1, 0)]
-
-grid = [[] for i in range(H*W)]
-
-def getnum(x, y, H, W):
+def get_num(x, y, W):
     return x*W + y
 
-def rec(v, dv, seen):
-    seen[v] = True
-    for n in dv[v]:
-        if seen[n] == True:
-            continue
-        rec(n, dv, seen)
-    return
-    
-def makegrid(A, grid, H, W, move):
-    for i in range(H):
-        for j in range(W):
-            if A[i][j] == '.':
-                continue
-            for dx, dy in move:
-                nx, ny = i+dx, j+dy
-                if 0<= nx <H and 0 <= ny < W and A[nx][ny] == '#':
-                    grid[getnum(i, j, H, W)].append(getnum(nx, ny, H, W))
-    return grid
-                    
-visited = [False] * (H*W)
+tree = [[] for _ in range(H*W)]
 
-dv = makegrid(A, grid, H , W, move)
+move = [(0, 1), (-1, 0), (0, -1), (1, 0)]
 
 for i in range(H):
     for j in range(W):
         if A[i][j] == '.':
             continue
-        rec(getnum(i, j, H, W), dv, visited)
+        for dx, dy in move:
+            ni, nj = i + dx, j + dy
+            if 0<=ni<H and 0<=nj<W and A[ni][nj] == '#':
+                tree[get_num(i, j, W)].append(get_num(ni, nj, W))
+    
 
-print(visited)
+visited = [False] * (H*W)
+
+
+def rec(v, dv, seen):
+    seen[v] = True
+    for j in dv[v]:
+        if seen[j] == True:
+            continue
+        rec(j, dv, seen)
+    return
+
+ans = 0
+for i in range(H):
+    for j in range(W):
+        if A[i][j] == '.':
+            continue
+        if visited[get_num(i, j, W)] == True:
+            continue
         
+        ans += 1
+        rec(get_num(i, j, W), tree, visited)
+
+        
+print(ans)
+    
