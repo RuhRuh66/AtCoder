7d4e08da2b234228c144fcd385ebc1febc430b6f
RuhRuh66 <toruh@deluxe.ocn.ne.jp>
2024-01-23 07:53:15 +0900

    11 in progress added.

---
+++ Boot_camp_for_Beginners/Medium_100/11.py
@@ -0,0 +1,13 @@
+N, M  = map(int, input().split())
+
+A = []
+C = []
+
+
+for i in range(N):
+    a, b = map(int, input().split())
+    A.append([a, b])
+for j in range(M):
+    c, d = map(int, input().split())
+    C.append([c, d])
+    
