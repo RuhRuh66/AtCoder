440ea5f0c9a35490717c734f986c0c267727fb8e
RuhRuh66 <toruh@deluxe.ocn.ne.jp>
2024-05-25 07:48:49 +0900

    44 in progress

--- Boot_camp_for_Beginners/Hard_100/44.py
+++ Boot_camp_for_Beginners/Hard_100/44.py
@@ -10,17 +10,32 @@ for j in range(N):
         x, y = map(int, input().split())
         queries[j+1].append((x,y))
 
+
+
 for i in range(1<<N):
-    assump = [[] for i in range(N+1)]
-    persons = [[] for i in range(N+1)]
+    assump = [0] * (N+1)
+    judge = [-1] * (N+1)
+   
     for j in range(N):
         if i>>j & 1 == 1:
-            for x, y in queries[j+1]:
-                persons[x].append(y)
-            assump[j+1].append(1)
-        else:
-            assump[j+1].append(0)
-    print(persons, assump)
+            assump[j+1] = 1
+            for x, y  in queries[j+1]:
+                judge[x] = y
     
-     
+    
+    
+    
+        
+          
+
+
+                    
+                    
             
+            
+            
+            
+    
+    
+    
+    
