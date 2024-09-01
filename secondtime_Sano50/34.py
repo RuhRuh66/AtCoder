54d312a659c247476841e466c9a584fcc5169bab
RuhRuh66 <toruh@deluxe.ocn.ne.jp>
2024-09-02 06:51:38 +0900

    34 modified

--- secondtime_Sano50/34.py
+++ secondtime_Sano50/34.py
@@ -1,52 +1,32 @@
-
-
-def eratosthenes(N):
-    prime = [True] * (N+1)
-    prime[0] = False
-    prime[1] = False
-    if N >= 4:
-        for i in range(4, N+1, 2):
-            prime[i] = False
-            
-        k = 3
-        while k*k <= N:
-            if prime[k] == True:
-                for t in range(2*k, N+1, k):
-                    prime[t] = False
-            else:
-                pass
-            k += 1
-            
-    lis = [x for x, t in enumerate(prime) if t == True]
-    return lis
-            
 N = int(input())
-
-sam = eratosthenes(N)   
 from collections import defaultdict
 
-fac = defaultdict(int)
+p = defaultdict(int)
 
-for n in sam:
-    while N > 1:
-        if N % n == 0:
-            fac[n] += 1
-            N //= n
-        else:
-            break
+i = 2
 
+while i*i <= N:
+    while N % i == 0:
+        p[i] += 1
+        N //= i
+    i += 1
+    
+        
+if N != 1:
+    p[N] += 1
+    
 cnt = 0
 
-for x, y  in fac.items():
-    i = 1
-    while y-i >= 0:
-        y -= i
-        cnt += 1
-        i += 1
+for i, j in p.items():
+    k = 1
+    while j>0:
+        cnt +=1
+        k += 1
+        j -= k
         
+
 print(cnt)
-            
-            
+        
 
         
 
