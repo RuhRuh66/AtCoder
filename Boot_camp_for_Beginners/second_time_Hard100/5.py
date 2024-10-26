97a0219e199578e4f8c0b91fc6980432c5ceb401
Toru Hara <toruh@deluxe.ocn.ne.jp>
2024-10-26 12:54:41 +0900

    5.6 added

---
+++ Boot_camp_for_Beginners/second_time_Hard100/5.py
@@ -0,0 +1,24 @@
+Sp = input()
+T = input()
+
+s = len(Sp)
+t = len(T)
+
+for i in range(s-t, -1, -1):
+    target = Sp[i:i+t]
+    flag = True
+    for j in range(t):
+        if target[j] == T[j]:
+            continue
+        elif target[j] == '?':
+            continue
+        else:
+            flag = False
+            break
+    if flag == True:
+        pre_ans = Sp[:i] + T + Sp[i+t:]
+        ans = pre_ans.replace('?', 'a')
+        print(ans)
+        exit()
+        
+print('UNRESTORABLE')
